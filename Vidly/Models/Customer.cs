ee2788db74060b4393284179a6536963c06a1866
codeflix <codeflix@lenovo910>
2017-05-14 12:20:36 -0400

    Add a form to add/update a customer.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,9 +13,15 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
+
         public byte MembershipTypeId { get; set; }
     }
 }
